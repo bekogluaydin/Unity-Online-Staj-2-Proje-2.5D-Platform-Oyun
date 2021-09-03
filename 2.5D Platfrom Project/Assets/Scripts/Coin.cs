e24ebc464c2d8ab4209f69c439f68599df870bc7
Aydın BEKOĞLU <aydinbek97@gmail.com>
2021-09-03 19:23:56 +0300

    update level
    update level

--- 2.5D Platfrom Project/Assets/Scripts/Coin.cs
+++ 2.5D Platfrom Project/Assets/Scripts/Coin.cs
@@ -13,7 +13,8 @@ public class Coin : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        //transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
